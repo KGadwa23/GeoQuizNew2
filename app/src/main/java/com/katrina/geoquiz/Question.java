7c5a253e6da470a078018ff15b6dcc2698f5486c
KG <kg@gmail.com>
2017-09-06 20:00:01 -0500

    Initial Set Up of GeoQuiz

---
+++ app/src/main/java/com/katrina/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.katrina.geoquiz;
+
+/**
+ * Created by katri on 9/6/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+}
